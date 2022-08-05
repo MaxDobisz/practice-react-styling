7939ef3dc3452327a1a3363628986eef48334b6a
Max <maciejdobisz@gmail.com>
2022-08-05 10:05:39 +0100

    task 01 finish

--- src/components/Alert/Alert.styled.js
+++ src/components/Alert/Alert.styled.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
 
-const StyledAlert = styled.div`
-    display: block;
+const DefaultStyles = styled.div`
+    background-color: yellow;
+    display: block; 
+    padding: 15px 25px;
+    border: 1px solid rgb(184 218 255);
+    border-radius: 5px;
+`
+
+const StyledAlert = styled(DefaultStyles)`
+    background-color: ${ props => props.theme[props.variant]};
 `
 
 export { StyledAlert };
